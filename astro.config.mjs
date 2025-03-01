b7f5e166aea2efc896d07b271d29fec2a812e2ea
Mufiidz <melody+dev@slmail.me>
2025-03-01 09:10:35 +0700

    upgrade tailwind v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel';
+import vercel from "@astrojs/vercel";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  output: 'server',
+  output: "server",
   adapter: vercel(),
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
